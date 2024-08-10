afc6eaa4d39c936f4266f94a06f5ce5eb5dfa791
tanfuhua <2813223276@qq.com>
2024-08-10 14:20:16 +0800

    feat:vercal端删除nginx的配置

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,6 @@
 export default {
     // baseURL: "https://erp.tanfuhua.com/"+"api/",  // 使用服务器的后端
     // baseURL: "http://127.0.0.1:8081/" + "api/", // 使用本地的后端
-    baseURL: "/api/", // 部署用的后端
+    // baseURL: "/api/", // 部署用的后端
+    baseURL: "https://erpapi.tanfuhua.com/api/", // vercal用的后端
 }
